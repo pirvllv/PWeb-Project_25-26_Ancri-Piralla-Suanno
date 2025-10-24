7fd10e8afe2abce654c3d34255cbd93b0281be3d
Lorenzo Piralla <lorenzo.piralla@studenti.unimi.it>
2025-10-24 15:09:13 +0200

    Correzioni SQL e completato documento
    Corrette le create.sql, completato il capitolo 2 e quindi tutto il documento

--- sql/create.sql
+++ sql/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Iscritto (
-    Email VARCHAR(100) NOT NULL PRIMARY KEY,
+    Email VARCHAR(255) NOT NULL PRIMARY KEY,
     Cognome VARCHAR(100) NOT NULL,
     Nome VARCHAR(100) NOT NULL,
     DataNascita DATE NOT NULL,
@@ -10,94 +10,92 @@ CREATE TABLE Iscritto (
 );
 
 CREATE TABLE Responsabile (
-    ResponsabileEmail VARCHAR(255) NOT NULL, -- vincolo solo docente può essere resp. non esprimibile
+    ResponsabileEmail VARCHAR(255) NOT NULL, -- vincolo solo docente può essere responsabile espresso in backend
     InizioIncarico DATE NOT NULL,
 
     FOREIGN KEY (ResponsabileEmail) REFERENCES Iscritto(Email)
-        ON DELETE NO ACTION
+        ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
 CREATE TABLE Settore (
     Nome VARCHAR(50) NOT NULL PRIMARY KEY,
     Tipologia VARCHAR(50) NOT NULL,
-    ResponsabileEmail VARCHAR(255) NOT NULL,
+    ResponsabileEmail VARCHAR(255),
 
     CONSTRAINT chk_TipologiaSettore CHECK (Tipologia IN ('danza', 'musica', 'teatro')),
     FOREIGN KEY (ResponsabileEmail) REFERENCES Responsabile(ResponsabileEmail)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE Iscrizione (
     IscrittoEmail VARCHAR(255) NOT NULL,
-    SettoreNome VARCHAR(100) NOT NULL,
+    SettoreNome VARCHAR(50) NOT NULL,
 
     PRIMARY KEY (IscrittoEmail, SettoreNome),
     FOREIGN KEY (IscrittoEmail) REFERENCES Iscritto(Email)
-        ON DELETE NO ACTION
+        ON DELETE CASCADE
         ON UPDATE CASCADE,
     FOREIGN KEY (SettoreNome) REFERENCES Settore(Nome)
-        ON DELETE NO ACTION
+        ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
 CREATE TABLE SalaProve (
     NumAula VARCHAR(50) NOT NULL PRIMARY KEY,
     Capienza INT NOT NULL CHECK (Capienza > 0),
-    SettoreNome VARCHAR(100) NOT NULL,
+    SettoreNome VARCHAR(50),
 
     FOREIGN KEY (SettoreNome) REFERENCES Settore(Nome)
-        ON DELETE NO ACTION
+        ON DELETE SET NULL
         ON UPDATE CASCADE
 );
 
 CREATE TABLE Strumentazione (
     ID INT AUTO_INCREMENT PRIMARY KEY,
-    NumAula VARCHAR(15) NOT NULL,
+    NumAula VARCHAR(15),
     Tipologia VARCHAR(50) NOT NULL,
     Descrizione VARCHAR(300),
 
     CONSTRAINT chk_TipologiaStrumentazione CHECK (Tipologia IN ('strumenti musicali', 'impianti audio', 'specchi', 'palcoscenico')),
 
     FOREIGN KEY (NumAula) REFERENCES SalaProve(NumAula)
-        ON DELETE CASCADE
+        ON DELETE SET NULL
         ON UPDATE CASCADE
 );
 
-ALTER TABLE Strumentazione AUTO_INCREMENT=1;
-
-CREATE TABLE Prenotazione (
+CREATE TABLE Prenotazione (     -- il vincolo di non sovrapposizione verrà imposto in backend
     ID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    DataPren DATE NULL DEFAULT NULL,
-    OraInizio TIME NULL DEFAULT NULL, -- ? come mai inizializzi con NULL DEFAULT NULL?
-    OraFine TIME NULL DEFAULT NULL,
-    Attivita VARCHAR(50),
+    DataPren DATE NOT NULL,
+    OraInizio TIME NOT NULL,
+    OraFine TIME NOT NULL,
+    Attivita VARCHAR(50) NOT NULL,
     NumAula VARCHAR(50) NOT NULL,
-    ResponsabileEmail VARCHAR(100),
+    ResponsabileEmail VARCHAR(255) NOT NULL,
 
     CONSTRAINT ChckDirTemp CHECK (OraFine > OraInizio),
 
     FOREIGN KEY (ResponsabileEmail) REFERENCES Responsabile(ResponsabileEmail)
         ON UPDATE CASCADE
-        ON DELETE NO ACTION,
+        ON DELETE CASCADE,
     FOREIGN KEY (NumAula) REFERENCES SalaProve(NumAula)
         ON UPDATE CASCADE
-        ON DELETE NO ACTION
+        ON DELETE CASCADE
 );
-
-ALTER TABLE Prenotazione AUTO_INCREMENT=1;
 
 CREATE TABLE Invito (
     PrenotazioneID INT NOT NULL,
-    IscrittoEmail VARCHAR(100) NOT NULL,
-    Accettazione BOOLEAN,
-    Motivazione VARCHAR(300),
-    DataRisposta TIMESTAMP,
+    IscrittoEmail VARCHAR(255) NOT NULL,
+    Accettazione BOOLEAN DEFAULT NULL,
+    Motivazione VARCHAR(300) DEFAULT NULL, -- da imporre in backend come NOT NULL quando viene data risposta negativa
+    DataRisposta DATETIME DEFAULT NULL,
     
     PRIMARY KEY(PrenotazioneID, IscrittoEmail),
 
     FOREIGN KEY (PrenotazioneID) REFERENCES Prenotazione(ID)
         ON UPDATE CASCADE
-        ON DELETE NO ACTION,
+        ON DELETE CASCADE,
     FOREIGN KEY (IscrittoEmail) REFERENCES Iscritto(Email)
         ON UPDATE CASCADE
         ON DELETE CASCADE
