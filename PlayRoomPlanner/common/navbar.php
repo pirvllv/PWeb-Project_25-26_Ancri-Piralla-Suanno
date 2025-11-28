7ecd81a3a58504e27095857aa0e8c08124cba307
Carlo Ancri <carlo.ancri@gmail.com>
2025-11-28 13:51:37 +0100

    ritagliata immagine e sistemati colori di sfondo + navbar

--- PlayRoomPlanner/common/navbar.php
+++ PlayRoomPlanner/common/navbar.php
@@ -4,27 +4,41 @@
       <div class="col-12">
         <nav class="main-nav">
 
-          <a href="/PlayRoomPlanner/index.php" class="logo">
-            <h1 style="color: white; margin: 0; line-height: inherit;">PlayRoomPlanner</h1>
-          </a>
-          <ul class="nav">
-            <li class="scroll-to-section"><a href="/PlayRoomPlanner/index.php" class="active">Home</a></li>
-            <li class="has-sub">
-              <a href="javascript:void(0)">Pagine</a>
-              <ul class="sub-menu">
-                <li><a href="#">Iscriviti</a></li>
-                <li><a href="/PlayRoomPlanner/frontend/chi_siamo.php">Chi siamo</a></li>
-                <li><a href="#">Contattaci</a></li>
-              </ul>
-            </li>
-            <?php if (isset($_SESSION['user_id'])): ?>
-              <li><a href="/frontend/area_personale.php">Area Personale</a></li>
-            <?php else: ?>
-              <li><a href="/frontend/login.php">Login</a></li>
-            <?php endif; ?>
-          </ul>
-        </nav>
-      </div>
+<head>
+    <meta charset="utf-8">
+    <title>Play Room Planner</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <link href="CSS/bootstrap.min.css" rel="stylesheet">
+    <link href="CSS/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-xxl bg-white p-0">
+        <div class="container-fluid bg-dark px-0">
+            <div class="row gx-0">
+                <div class="col-lg-3 bg-dark d-none d-lg-block">
+                    <a href="index.php"
+                        class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
+                        <h1 class="m-0 text-primary text-uppercase">PlayRoom</h1>
+                    </a>
+                </div>
+                <div class="col-lg-9">
+                    <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-3 p-lg-0">
+                        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                            <div class="navbar-nav mr-auto py-0">
+                                <a href="index.php" class="nav-item nav-link active">Home</a>
+                                <a href="#" class="nav-item nav-link">Iscrizioni</a>
+                            </div>
+                            <div class="d-flex align-items-center pe-4">
+                                <a href="#" class="btn btn-primary rounded-0 py-4 px-md-5">Area Personale</a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</header>
+</body>
+
+</html>
