a675e3dbd556935e40fe27eadf1a1cb3ef068fd3
Carlo Ancri <carlo.ancri@gmail.com>
2025-11-30 19:55:20 +0100

    implementata schermata home minimale

--- PlayRoomPlanner/index.php
+++ PlayRoomPlanner/index.php
@@ -1,69 +1,123 @@
-<?php
-include 'common/navbar.php';
-?>
-
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="css/style.css">
-
 <!DOCTYPE html>
 <html lang="it">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PlayRoomPlanner - Home</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/fontawesome.css">
+	<link rel="stylesheet" href="css/templatemo-574-mexant.css">
+	<link rel="stylesheet" href="css/owl.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+	<title>PlayRoomPlanner - Home</title>
 </head>
 
 <body>
+	<?php
+	include 'common/navbar.php';
+	?>
 
-    <div class="container mt-5 text-center">
-        <h1 class="display-4">Benvenuto in PlayRoomPlanner<?php if (isset($_SESSION['username'])) { echo ", " . htmlspecialchars($_SESSION['username']); } ?></h1>
+	<div class="swiper-container" id="top">
+		<div class="swiper-wrapper">
+			<div class="swiper-slide">
+				<div class="slide-inner" style="background-image: url(immagini/home_bg.jpg);">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-8">
+								<div class="header-text">
+									<h2>Benvenuto su <em>PlayRoomPlanner</em></h2>
+									<div class="div-dec"></div>
+									<p>Il servizio ideale per organizzare e gestire le tue stanze per provare. Prenota e pianifica eventi
+										nelle nostre sale.<br>Scorri verso il basso per scoprire i nostri servizi!</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-        <p class="lead">Mettiamo a disposizione tre settori adatti all'esigenza di ogni artista:</p>
+	<section class="services" id="services">
+		<div class="container">
+			<div class="row">
+				<!-- Danza -->
+				<div class="col-lg-12">
+					<div class="service-item">
+						<div class="row">
+							<div class="col-lg-4">
+								<div class="icon">
+									<img src="immagini/danza.jpg" alt="Danza">
+								</div>
+							</div>
+							<div class="col-lg-8">
+								<div class="right-content">
+									<h4>Danza</h4>
+									<p>Le nostre sale danza offrono spazi luminosi, pavimentazione ammortizzata, specchi a tutta parete e
+										impianti audio professionali. Sono progettate per garantire comfort, sicurezza e la massima qualità
+										di movimento, ideali per lezioni, prove, workshop e produzioni artistiche. Disponibili per scuole,
+										compagnie e singoli professionisti.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 
-        <hr class="my-4">
+				<!-- Musica -->
+				<div class="col-lg-12">
+					<div class="service-item">
+						<div class="row">
+							<div class="col-lg-4">
+								<div class="icon">
+									<img src="immagini/musica.jpg" alt="Musica">
+								</div>
+							</div>
+							<div class="col-lg-8">
+								<div class="right-content">
+									<h4>Musica</h4>
+									<p>Le nostre sale di registrazione e prova sono trattate acusticamente, dotate di strumentazione
+										professionale e pensate per garantire un ascolto preciso e un ambiente di lavoro stabile. Offrono
+										spazio, isolamento e qualità sonora per band, solisti, produttori e tecnici. Ideali per prove,
+										sessioni di recording, pre-produzioni e progetti didattici.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 
-        <div class="row">
-            <!-- box danza -->
-            <div class="col-md-4">
-                <div class="card text-white bg-dark mb-3"
-                    style="background-image: url('immagini/danza.jpg'); background-size: cover; height: 200px;">
-                    <div class="card-body d-flex align-items-center justify-content-center"
-                        style="background-color: rgba(146, 135, 135, 0.5);">
-                        <h3 class="card-title p-2 border border-light rounded-3 bg-light bg-opacity-50">Danza</h3>
-                    </div>
-                </div>
-            </div>
+				<!-- Teatro -->
+				<div class="col-lg-12">
+					<div class="service-item">
+						<div class="row">
+							<div class="col-lg-4">
+								<div class="icon">
+									<img src="immagini/teatro.jpg" alt="Teatro">
+								</div>
+							</div>
+							<div class="col-lg-8">
+								<div class="right-content">
+									<h4>Teatro</h4>
+									<p>Il nostro teatro è uno spazio versatile, con palco attrezzato, illuminotecnica professionale e
+										acustica curata. Offre un ambiente ideale per spettacoli, prove, rassegne, presentazioni ed eventi
+										culturali. Pensato per compagnie, scuole e realtà artistiche che necessitano di una scena affidabile
+										e pronta all'uso.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 
-            <!-- box musica -->
-            <div class="col-md-4">
-                <div class="card text-white bg-dark mb-3"
-                    style="background-image: url('immagini/musica.jpg'); background-size: cover; height: 200px;">
-                    <div class="card-body d-flex align-items-center justify-content-center"
-                        style="background-color: rgba(136, 125, 125, 0.5);">
-                        <h3 class="card-title p-2 border border-light rounded-3 bg-light bg-opacity-50">Musica</h3>
-                    </div>
-                </div>
-            </div>
 
-            <!-- box teatro -->
-            <div class="col-md-4">
-                <div class="card text-white bg-dark mb-3"
-                    style="background-image: url('immagini/teatro.jpg'); background-size: cover; height: 200px;">
-                    <div class="card-body d-flex align-items-center justify-content-center"
-                        style="background-color: rgba(180, 169, 169, 0.5);">
-                        <h3 class="card-title p-2 border border-light rounded-3 bg-light bg-opacity-50">Teatro</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
+	<?php
+	include 'common/footer.php';
+	?>
 </body>
 
 </html>
-
-<?php
-include 'common/footer.php';
-?>
