882f4c1cdb33f6fbad3ecf8954387ac21ab200a4
Carlo Ancri <carlo.ancri@gmail.com>
2025-11-30 19:54:24 +0100

    footer e navbar applicabili ad ogni pagina del sito

---
+++ PlayRoomPlanner/common/footer.php
@@ -0,0 +1,9 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <p>Progetto Programmazione per il Web: PlayRoom Planner. Ancri, Piralla, Suanno.</p>
+      </div>
+    </div>
+  </div>
+</footer>
