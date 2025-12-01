c0d8a33ad8a65ac1eaa4fc35736e89dfd0229d5a
Carlo Ancri <carlo.ancri@gmail.com>
2025-12-01 14:27:21 +0100

    importati file navbar.php e footer.php

--- PlayRoomPlanner/common/footer.php
+++ PlayRoomPlanner/common/footer.php
@@ -1,13 +1,9 @@
-</main>
-
-<footer class="bg-light text-center text-lg-start mt-5 p-4 border-top">
-  <div class="text-center p-3">
-    Play Room Planner - Progetto Programmazione Web di Ancri, Piralla, Suanno.
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <p>Progetto di Programmazione per il Web: PlayRoomPlanner. A cura di Ancri, Piralla, Suanno.</p>
+      </div>
+    </div>
   </div>
 </footer>
-
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="js/script.js"></script>
-</body>
-
-</html>
---
+++ PlayRoomPlanner/common/navbar.php
@@ -0,0 +1,33 @@
+<header class="header-area header-sticky">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <nav class="main-nav">
+
+          <a href="index.html" class="logo">
+            <h1 style="color: white; margin: 0; line-height: inherit;">PlayRoomPlanner</h1>
+          </a>
+          <ul class="nav">
+            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+            <li class="has-sub">
+              <a href="javascript:void(0)">Pagine</a>
+              <ul class="sub-menu">
+                <li><a href="#">Iscriviti</a></li>
+                <li><a href="#">Chi siamo</a></li>
+                <li><a href="#">Contattaci</a></li>
+              </ul>
+            </li>
+            <?php if (isset($_SESSION['user_id'])): ?>
+              <li><a href="area_personale.php">Area Personale</a></li>
+            <?php else: ?>
+              <li><a href="login.php">Login</a></li>
+            <?php endif; ?>
+          </ul>
+          <a class='menu-trigger'>
+            <span>Menu</span>
+          </a>
+        </nav>
+      </div>
+    </div>
+  </div>
+</header>
