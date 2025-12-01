728056741609fee984151e6edc1a12e83fa25753
Carlo Ancri <carlo.ancri@gmail.com>
2025-12-01 14:19:34 +0100

    corretta frase nel footer

--- PlayRoomPlanner/common/footer.php
+++ PlayRoomPlanner/common/footer.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-12">
-        <p>Progetto Programmazione per il Web: PlayRoom Planner. Ancri, Piralla, Suanno.</p>
+        <p>Progetto di Programmazione per il Web: PlayRoomPlanner. A cura di Ancri, Piralla, Suanno.</p>
       </div>
     </div>
   </div>
