c3e63cd3e1f82ea94b2157a20383600e7a0bc377
Carlo Ancri <carlo.ancri@gmail.com>
2025-12-02 10:26:12 +0100

    aggiunta pagina chi_siamo.php in frontend - da implementare

---
+++ PlayRoomPlanner/frontend/chi_siamo.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap">
+  <link rel="stylesheet" href="/PlayRoomPlanner/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/PlayRoomPlanner/css/fontawesome.css">
+  <link rel="stylesheet" href="/PlayRoomPlanner/css/templatemo-574-mexant.css?v=<?php echo time(); ?>">
+  <link rel="stylesheet" href="/PlayRoomPlanner/css/owl.css">
+  <link rel="stylesheet" href="/PlayRoomPlanner/css/animate.css">
+	<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+	<title>PlayRoomPlanner - Chi Siamo</title>
+</head>
+
+<body>
+  <?php 
+    include '../common/navbar.php'; 
+  ?>
+
+  <div class="page-heading">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="header-text">
+            <h2>Chi siamo</h2>
+            <div class="div-dec"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php 
+    include '../common/footer.php';
+  ?>
+</body>
+
+</html>
