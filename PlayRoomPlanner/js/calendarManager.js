84039686f4952499386a9cf1efbd490ec3d07a9d
Leandro <leandro.suanno@live.com>
2026-01-14 23:07:54 +0100

    Fix su inviti in sospeso
    Prima a volte venivano rimossi dopo aver caricato la pagina

--- PlayRoomPlanner/js/calendarManager.js
+++ PlayRoomPlanner/js/calendarManager.js
@@ -111,14 +111,22 @@ function displayAtt(att) {
     //console.log(att);
     if (att["orafine"]<=att["orainizio"]) {return "";}
     
-    out += "<div class=\"cell " + att["stato"]+ "\" ";
+    out += "<div class=\"cell " + att["stato"]+ " att\" ";
     if(att["stato"]==="attRifiutata") {
         out += "style='display: none;'";
     }
     out += ">";
-    out += att["attivita"] + "</div>";
+    out += att["attivita"];
+    out += "<span class='invites-icons'>";
+    out += '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">'
+        + '<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>'
+        + '</svg>';
+    out += '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">'
+        + '<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>'
+        + '</svg>';
+    out += "</span></div>";
 
-    //console.log("Att: "+out)
+    console.log("Att: "+out)
     return out;
     
 }
@@ -128,19 +136,25 @@ function scroll_from_invites(inviti) {
     //console.log(week);
     //console.log(inviti);
     let scroll = document.getElementById("scroll");
+    //console.log(scroll);
+    let scrollhtml = "";
+    //console.log(inviti);
+
     for (const g in inviti) {
         //console.log(week[g]);
-        let daycont = document.createElement('div');
-        daycont.setAttribute("id", g);
-        daycont.innerHTML += "<div class=\"cell index\">"+inviti[g]["wkday"]+"</div>";
+        let dayconthtml = "<div id='"+ g +"'>";
+        let invithtml = "<div class=\"cell index\">"+inviti[g]["wkday"]+"</div>";
         for (const attIdx in inviti[g]["attivita"]) {
-            daycont.innerHTML += displayAtt(inviti[g]["attivita"][attIdx]);
+            invithtml += displayAtt(inviti[g]["attivita"][attIdx]);
+            console.log("Attività aggiunta");
         }
-        scroll.appendChild(daycont);
-        //daycont.innerHTML += "<br>";
+        dayconthtml += invithtml + "</div>";
+        scrollhtml += dayconthtml;
+        //console.log(scroll.innerHTML);
 
     }
-    //console.log(scroll.innerHTML);
+    scroll.innerHTML = scrollhtml;
+    console.log(scroll.innerHTML);
     return;
     
 }
@@ -159,7 +173,7 @@ function clearTable() {
 
     //console.log("cleartable");
 
-    let activities = document.getElementsByClassName("att");
+    let activities = document.getElementsByClassName("attAccettata");
     //console.log(activities);
     while(activities.length != 0) {
         //console.log(i);
